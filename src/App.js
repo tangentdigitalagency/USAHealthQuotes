e021865f5bce6eb26982379736460b12bf7b93ba
Abdullah108 <abdullahshahid.ascs@gmail.com>
2021-03-23 00:08:50 +0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Main from './Main';
+import Assurance from './Component/Assurance';
+// import "./index.css";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import ContextApiProvider from './context';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<>
+			<ContextApiProvider>
+				<Switch>
+					<Route exact path='/' component={Main} />
+					<Route exact path='/assurance' component={Assurance} />
+					<Redirect to='/' />
+				</Switch>
+			</ContextApiProvider>
+		</>
+	);
+};
 
 export default App;
